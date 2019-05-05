b27308b9e9fa3fad6e278cb4928e992f7d2d94b3
facccge <fcfc5702@qq.com>
2019-05-05 22:05:59 +0800

    Completed Company entity and Employee entity

--- src/main/java/com/example/employee/entity/Company.java
+++ src/main/java/com/example/employee/entity/Company.java
@@ -1,5 +1,32 @@
 package com.example.employee.entity;
 
-public class Company {
+import javax.persistence.Entity;
+import java.io.Serializable;
 
+@Entity
+public class Company implements Serializable {
+    private int id;
+    private String companyName;
+    private int employeesNumber;
+
+    public Company() {
+    }
+
+    public Company(int id, String companyName, int employeesNumber) {
+        this.id = id;
+        this.companyName = companyName;
+        this.employeesNumber = employeesNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public int getEmployeesNumber() {
+        return employeesNumber;
+    }
 }
--- src/main/java/com/example/employee/entity/Employee.java
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -1,5 +1,52 @@
 package com.example.employee.entity;
 
-public class Employee {
+import javax.persistence.Entity;
+import java.io.Serializable;
 
+/**
+ * @author Philadelphia
+ */
+@Entity
+public class Employee implements Serializable{
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+    private int companyId;
+    private int salary;
+
+    public Employee(){}
+
+    public Employee(int id, String name, int age, String gender, int companyId, int salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.companyId = companyId;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getCompanyId() {
+        return companyId;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
 }
